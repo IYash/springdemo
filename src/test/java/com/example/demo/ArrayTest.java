a033bdb656efdb3c18a60670f885ae18e1f06a57
shiguang <shiguang@xiaohongshu.com>
2021-07-08 20:33:09 +0800

    target position

--- src/test/java/com/example/demo/ArrayTest.java
+++ src/test/java/com/example/demo/ArrayTest.java
@@ -56,7 +56,7 @@ public class ArrayTest {
     }
     @Test
     public void test2(){
-        int[] nums =new int[]{2,4,3};
+        int[] nums =new int[]{1,4,3};
         nums = nextSeq(nums);
         System.out.println(nums[0]+"--"+nums[1]+"--"+nums[2]);
     }
@@ -97,5 +97,61 @@ public class ArrayTest {
         int temp = nums[left];
         nums[left] = nums[right];
         nums[right] = temp;
+    }
+    @Test
+    public void test3(){
+        int[] nums = new int[]{1,2,3};
+        int len = nums.length;
+        int[] result = findTarget(nums,0,len-1,1);
+        System.out.println(result[0]+"---"+result[1]);
+    }
+    //在排序数组中查找给定目标值的开始位置和结束位置,考虑二分法
+    private int[] findTarget(int[] nums,int left,int right,int target){
+        if(left>=right){
+            if(nums[left] == target) return new int[]{left,left};
+            else return new int[]{-1,-1};
+        }
+        int mid = (left+right)>>1;
+        int[] midT ;
+        int[] leftT ;
+        int[] rightT ;
+        int midV = nums[mid];
+        if(midV == target) {
+            midT = new int[]{mid, mid};
+            rightT = findTarget(nums,mid+1,right,target);
+            leftT = findTarget(nums,left,mid-1,target);
+            return combineResult(leftT,midT,rightT);
+        }else if(midV < target){
+            return findTarget(nums,mid+1,right,target);
+        }else{
+            return findTarget(nums,left,mid-1,target);
+        }
+    }
+
+    private int[] combineResult(int[] leftT, int[] midT, int[] rightT) {
+        int left = -1;
+        int right = -1;
+        if(midT[0]>0){
+            left = midT[0];
+            right = midT[1];
+            if(leftT[0]>=0){
+                left = leftT[0];
+            }
+            if(rightT[1]>=0){
+                right = rightT[1];
+            }
+        }else{
+            if(leftT[0]>=0) return leftT;
+            if(rightT[0]>=0) return rightT;
+        }
+        return new int[]{left,right};
+    }
+    @Test
+    public void test4(){
+        int[] leftT = new int[]{-1,-1};
+        int[] midT = new int[]{5,5};
+        int[] rightT = new int[]{6,8};
+        int[] result = combineResult(leftT,midT,rightT);
+        System.out.println(result[0]+"---"+result[1]);
     }
 }
