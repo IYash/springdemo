f2d7a3ed825a76b247cc03bceb4133812f9aa300
shiguang <shiguang@xiaohongshu.com>
2021-08-23 11:39:28 +0800

    update

--- src/main/java/com/example/demo/annotation/AnnotationInjected.java
+++ src/main/java/com/example/demo/annotation/AnnotationInjected.java
@@ -1,18 +1,18 @@
-package com.example.demo.annotation;
-
-
-import com.example.PrintMe;
-
-/**
- * @Author: shiguang
- * @Date: 2021/8/14
- * @Description:
- **/
-@PrintMe
-public class AnnotationInjected {
-    @Override
-    public String toString() {
-        System.out.println("annotationInjected");
-        return super.toString();
-    }
-}
+//package com.example.demo.annotation;
+//
+//
+//import com.example.PrintMe;
+//
+///**
+// * @Author: shiguang
+// * @Date: 2021/8/14
+// * @Description:
+// **/
+//@PrintMe
+//public class AnnotationInjected {
+//    @Override
+//    public String toString() {
+//        System.out.println("annotationInjected");
+//        return super.toString();
+//    }
+//}
--- src/main/java/com/example/demo/annotation/BoundInfoInjected.java
+++ src/main/java/com/example/demo/annotation/BoundInfoInjected.java
@@ -1,16 +1,16 @@
-package com.example.demo.annotation;
-
-import com.example.BoundInfo;
-
-/**
- * @Author: shiguang
- * @Date: 2021/8/14
- * @Description:
- **/
-@BoundInfo("annotationInfo")
-public class BoundInfoInjected {
-
-    private String boundInfo;
-    @BoundInfo
-    public void printHello(){}
-}
+//package com.example.demo.annotation;
+//
+//import com.example.BoundInfo;
+//
+///**
+// * @Author: shiguang
+// * @Date: 2021/8/14
+// * @Description:
+// **/
+//@BoundInfo("annotationInfo")
+//public class BoundInfoInjected {
+//
+//    private String boundInfo;
+//    @BoundInfo
+//    public void printHello(){}
+//}
