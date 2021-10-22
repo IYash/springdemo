f5088cb1932449becf7d6290cadd888aaba05100
shiguang <shiguang@xiaohongshu.com>
2021-10-22 19:22:31 +0800

    update

--- src/test/java/com/example/demo/TempTest.java
+++ src/test/java/com/example/demo/TempTest.java
@@ -2,10 +2,15 @@ package com.example.demo;
 
 import com.example.demo.util.SnowFlakeShortUrl;
 import com.google.common.collect.Lists;
+import com.site.lookup.util.StringUtils;
 import org.junit.Test;
+import org.springframework.util.CollectionUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -103,5 +108,100 @@ public class TempTest {
         }
         System.out.println(System.currentTimeMillis()-start);
     }
+    @Test
+    public void quantileTest(){
+        String source = "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]";
+        int len1 = source.length();
+        Double key = 4.0;
+        String[] ss = source.substring(1,len1-1).split(",");
+        int len = ss.length;
+        Double[] nums = new Double[len];
+        double rate = 100 / len;
+        //将string转成double
+        for (int i=0;i<len;i++){
+            nums[i] = Double.valueOf(ss[i]);
+        }
+        int base = 2;
+        double inputVal = Double.valueOf(key);
+        if (key<nums[0]) {
+            System.out.println(rate);
+        } else if (key>nums[len-1]) {
+            System.out.println(99.9);
+        }
+        int[] index = closetSearch(nums,inputVal,0,len-2);
+//        if ((nums[index[0]] + nums[index[1]] - base * inputVal) < 0) {
+//            System.out.println((index[0]+1) * rate);
+//        } else {
+//            System.out.println((index[1]+1) * rate);
+//        }
+        System.out.println(index[0]+"--->"+index[1]);
+        System.out.println(inputVal-nums[index[1]]);
+        System.out.println(nums[index[0]]-nums[index[1]]);
+        if (nums[index[0]]-nums[index[1]] == 0) {
+            System.out.println((index[1]+1)*rate);
+        }else{
+        System.out.println((index[1]+1+(inputVal-nums[index[1]])/(nums[index[0]]-nums[index[1]]))*rate);}
+    }
+    private boolean binarySearch(Double[] nums,double k,int start,int end){
+        int mid = 0;
+        double cur = 0.0;
+        while (start <= end) {
+            mid = (start + end) >> 1;
+            cur = nums[mid];
+            if (cur == k) {
+                return true;
+            } else {
+                if (cur < k) {
+                    start = mid + 1;
+                } else{
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+    private int[] closetSearch(Double[] nums,double k,int start,int end){
+        int mid = 0;
+        double cur = 0.0;
+        int left = start;
+        int right = end;
+        while (start <= end) {
+            mid = (start + end) >> 1;
+            cur = nums[mid];
+            if (cur == k) {
+                return new int[]{mid,mid};
+            } else {
+                if (cur < k) {
+                    start = mid + 1;
+                    left = start;
+                } else{
+                    end = mid-1;
+                    right = end;
+                }
+            }
+        }
+        return new int[]{left,right};
+    }
 
+    class ComparingO{
+        int priority;
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public void setPriority(int priority) {
+            this.priority = priority;
+        }
+    }
+    @Test
+    public void comparingTest(){
+        ComparingO o1 = new ComparingO();
+        o1.setPriority(2);
+        ComparingO o2 = new ComparingO();
+        o2.setPriority(1);
+        List<ComparingO> list = Lists.newArrayList(o1,o2);
+        Collections.sort(list, Comparator.comparing(ComparingO::getPriority));
+        System.out.println(list.get(0).getPriority());
+    }
 }
